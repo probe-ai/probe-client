700956ebbab1d6b30f609f83c8dd75a6b4115386
tanmay000009 <tanmayvyas009@gmail.com>
2023-02-23 15:33:22 +0530

    add sample snowflake util

---
+++ src/util/snowflake.util.ts
@@ -0,0 +1,44 @@
+import { Logger } from '@nestjs/common';
+import { createConnection } from 'snowflake-sdk';
+
+export class SnowflakeUtil {
+  private connection: any;
+
+  constructor() {
+    this.verifyEnv();
+
+    // todo: add option for connection pool
+    try {
+      this.connection = createConnection({
+        account: process.env.SNOWFLAKE_ACCOUNT,
+        username: process.env.SNOWFLAKE_USERNAME,
+        password: process.env.SNOWFLAKE_PASSWORD,
+        database: process.env.SNOWFLAKE_DATABASE,
+        schema: process.env.SNOWFLAKE_SCHEMA,
+        warehouse: process.env.SNOWFLAKE_WAREHOUSE,
+      });
+    } catch (error) {
+      throw new Error(`Error connecting to Snowflake: ${error.message}`);
+    }
+  }
+
+  public async getAllTableNames() {}
+  public async getDbMetadataForTables() {}
+  public async toDbMetadataDto() {}
+  public async queryDB() {}
+
+  private verifyEnv() {
+    if (
+      !process.env.SNOWFLAKE_ACCOUNT ||
+      !process.env.SNOWFLAKE_USERNAME ||
+      !process.env.SNOWFLAKE_PASSWORD ||
+      !process.env.SNOWFLAKE_DATABASE ||
+      !process.env.SNOWFLAKE_SCHEMA ||
+      !process.env.SNOWFLAKE_WAREHOUSE
+    ) {
+      throw new Error(
+        'Snowflake environment variable are not set. Please refer .env.sample',
+      );
+    }
+  }
+}
