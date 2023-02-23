52b5ecb1cb3e756635051b22f43afeb938d0d159
tanmay000009 <tanmayvyas009@gmail.com>
2023-02-23 23:57:45 +0530

    fix snowflake connection

--- src/client-db/util/snowflake.util.ts
+++ src/client-db/util/snowflake.util.ts
@@ -10,7 +10,7 @@ export class SnowflakeUtil implements IClientDb {
 
     // todo: add option for connection pool
     try {
-      this.connection = createConnection({
+      const connection = createConnection({
         account: process.env.SNOWFLAKE_ACCOUNT,
         username: process.env.SNOWFLAKE_USERNAME,
         password: process.env.SNOWFLAKE_PASSWORD,
@@ -18,14 +18,47 @@ export class SnowflakeUtil implements IClientDb {
         schema: process.env.SNOWFLAKE_SCHEMA,
         warehouse: process.env.SNOWFLAKE_WAREHOUSE,
       });
+
+      connection.connect((err, conn) => {
+        if (err) {
+          console.log(err);
+
+          throw new Error(`Error connecting to Snowflake: ${err}`);
+        }
+        this.connection = conn;
+        Logger.log('Connected to Snowflake');
+      });
     } catch (error) {
       throw new Error(`Error connecting to Snowflake: ${error.message}`);
     }
   }
 
-  public async getAllTableNames() {}
+  public async getAllTableNames() {
+    const query = `select * from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1`;
+    const result = await this.queryDB(query);
+    console.log(result);
+    return result;
+  }
   public async getDbMetadataForTables() {}
-  public async queryDB() {}
+
+  public async queryDB(query: string) {
+    try {
+      const result = await this.connection.execute({
+        sqlText: query,
+        complete: (err, stmt, rows) => {
+          if (err) {
+            throw new Error(`Error querying Snowflake: ${err}`);
+          }
+          return rows;
+        },
+      });
+      console.log(result);
+
+      return result;
+    } catch (error) {
+      throw new Error(`Error querying Snowflake: ${error.message}`);
+    }
+  }
 
   private verifyEnv() {
     if (
