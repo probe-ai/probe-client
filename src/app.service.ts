f1e33dd69befc94d95b10b11e662c281099e8404
tanmay000009 <tanmayvyas009@gmail.com>
2023-02-23 15:32:30 +0530

    add common util type

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,42 +7,50 @@ import {
   DbMetadataResDto,
 } from './dto/tables.dto';
 import { BigQueryUtil } from './util/bigquery.util';
+import { SnowflakeUtil } from './util/snowflake.util';
 
 @Injectable()
 export class AppService {
   private probeType: ProbeType;
+  private util: SnowflakeUtil | BigQueryUtil;
 
   constructor() {
     const probeType = process.env.PROBE_TYPE;
 
-    this.probeType = probeType as ProbeType;
+    this.probeType = ProbeType[probeType as keyof typeof ProbeType];
 
     if (!this.probeType) {
       throw new Error(
-        'TYPE environment variable is not set. Please refer .env.sample',
+        'PROBE_TYPE environment variable is not set. Please refer .env.sample',
       );
+    }
+
+    if (this.probeType === ProbeType.BIGQUERY) {
+      this.util = new BigQueryUtil();
+    } else if (this.probeType === ProbeType.SNOWFLAKE) {
+      this.util = new SnowflakeUtil();
     }
   }
 
-  public async getAllTableNames(): Promise<AllTablesResDto> {
-    const tableNames = await BigQueryUtil.getAllTableNames();
+  public async getAllTableNames() { // : Promise<AllTablesResDto>
+    const tableNames = await this.util.getAllTableNames();
 
     return { tableNames };
   }
 
   public async getDbMetadata(
-    reqDto: DbMetadataReqDto,
-  ): Promise<DbMetadataResDto> {
-    const metadata = await BigQueryUtil.getDbMetadataForTables(
-      reqDto.tableNames,
-    );
+    reqDto: DbMetadataReqDto, // : Promise<DbMetadataResDto>
+  ) {
+    const metadata = await this.util.getDbMetadataForTables(reqDto.tableNames);
 
     return { metadata };
   }
 
-  public async runQuery(reqDto: RunQueryReqDto): Promise<RunQueryResDto> {
+  public async runQuery(
+    reqDto: RunQueryReqDto, // : Promise<RunQueryResDto>
+  ) {
     try {
-      const queryResults = await BigQueryUtil.queryDB(reqDto.query);
+      const queryResults = await this.util.queryDB(reqDto.query);
 
       return { status: 'success', queryResults };
     } catch (error) {
